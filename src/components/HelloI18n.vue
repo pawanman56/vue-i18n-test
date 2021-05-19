7c5dd29ede46fae075ec6dcaff2f61bada34c34a
Pawan Sunuwar <pawanman56@gmail.com>
2021-05-19 17:25:56 +0545

    added vue-i18n

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <p>{{ $t('hello') }}</p>
+  <p>{{ $t('message') }}</p>
+</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelloI18n'
+})
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  },
+  "es": {
+    "hello": "Hola!"
+  }
+}
+</i18n>
